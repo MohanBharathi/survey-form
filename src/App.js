713991e2e1f2aa40942ceb65f75fcd3c7ed8dbd8
MohanBharathi <mohanbharathi1712@gmail.com>
2022-10-10 15:42:05 +0530

    added further funcitons and css

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
+import './App.css';
 
 function App() {
-  // const [data, setData] = useState({
-  //   name: "",
-  //   age: "",
-  //   address: "",
-  //   img:imgUrl
-  // });
-  // const changeHandler = (e) => {
-  //   setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  // };
-
-  // const submitHandler = (e) => {
-  //   e.preventDefault();
-  //   console.log(data);
-  //   axios
-  //     .post(
-  //       "https://sheet.best/api/sheets/e9a48439-26c6-4aed-b144-2b83464e8672",
-  //       data
-  //     )
-  //     .then((res) => console.log(res));
-  // };
-
-
+  
 
   const [name,setName] = useState(null);
   const [age,setAge] = useState(null);
   const [address,setAddress] = useState(null)
   const [imgUrl,setImgUrl]=useState(null);
+  const [pdfUrl,setPdfUrl]=useState(null);
   const [phoneNo,setPhoneNo]=useState(null);
   const [email,setEmail]=useState(null);
-
 
   const [image,setImage]=useState(null);
   const [isPending,setIsPending]=useState(false);
   const [error,setError]=useState(false);
   const [errMsg,setErrMsg]=useState('')
+
+
+
+  const [data, setData] = useState({
+    name: "",
+    age: "",
+    address: "",
+    img:imgUrl,
+    phone:'',
+    email:'',
+  });
+  const changeHandler = (e) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const submitHandlerSheet = (e) => {
+    e.preventDefault();
+    console.log(data);
+    axios
+      .post(
+        "https://sheet.best/api/sheets/e9a48439-26c6-4aed-b144-2b83464e8672",
+        {data,imgUrl}
+      )
+      .then((res) => console.log(res));
+  };
 
   const ImageUpload=(data)=>{
     console.log(data,'data')
@@ -57,68 +62,103 @@ function App() {
       setIsPending(false)
     })
   }
-  const PdfUpload=(data)=>{
-    console.log(data,'data')
-    setIsPending(true);
-    setImage(data);
-    const formData=new FormData();
-      formData.append("file",data);
-      formData.append("upload_preset","azure_career");
-      formData.append("folder","pdf")
-      formData.append("public_id",data.name)
-      axios.post("https://api.cloudinary.com/v1_1/dcmztntur/image/upload/",formData)
-      .then(res=>{
-        setImgUrl(res.data.url)
-    }).then(()=>{setIsPending(false)}).catch(err=>{
-      console.log(err,'erer');
-      setErrMsg(err.message);
-      setError(true);
-      setIsPending(false)
-    })
+  // const PdfUpload=(data)=>{
+  //   console.log(data,'data')
+  //   setIsPending(true);
+  //   const formData=new FormData();
+  //     formData.append("file",data);
+  //     formData.append("upload_preset","azure_career");
+  //     formData.append("folder","pdf")
+  //     formData.append("public_id",data.name)
+  //     axios.post("https://api.cloudinary.com/v1_1/dcmztntur/image/upload/",formData)
+  //     .then(res=>{
+  //       setPdfUrl(res.data.url)
+  //   }).then(()=>{setIsPending(false)}).catch(err=>{
+  //     console.log(err,'error');
+  //     setErrMsg(err.message);
+  //     setError(true);
+  //     setIsPending(false)
+  //   })
+  // }
+  const PdfUpload = (e)=>{
+      setIsPending(true);
+      var file = e.target.files[0] //the file
+      var reader = new FileReader() //this for convert to Base64 
+      reader.readAsDataURL(e.target.files[0]) //start conversion...
+      reader.onload = function (e) { //.. once finished..
+        var rawLog = reader.result.split(',')[1]; //extract only thee file data part
+        var dataSend = { dataReq: { data: rawLog, name: file.name, type: file.type }, fname: "uploadFilesToGoogleDrive" }; //preapre info to send to API
+        fetch('https://script.google.com/macros/s/AKfycbzW_s7I8kH6j4V19wmPI3J7BchQmVytb_0zqOaNJAQ2EdKB5DlUoIOtOl_G6uWxNuuJhQ/exec', //your AppsScript URL
+          { method: "POST", body: JSON.stringify(dataSend) }) //send to Api
+          .then(res => res.json()).then((a) => {
+            console.log(a)
+            setPdfUrl(a) //See response
+            setIsPending(false)
+          }).catch(e => {
+            setErrMsg(e)
+            setIsPending(false)
+            console.log(e)}) // Or Error in console
+      
+    }
   }
-
-  const submitHandler = (e) => {
-      e.preventDefault();
-      console.log({name,age,address,imgUrl,phoneNo,email});
-      axios.post("http://localhost:3000/data",{name,age,address,imgUrl,phoneNo,email})
-  }
+//   const ImageUpload = (e)=>{
+//     var file = e.target.files[0] //the file
+//     var reader = new FileReader() //this for convert to Base64 
+//     reader.readAsDataURL(e.target.files[0]) //start conversion...
+//     reader.onload = function (e) { //.. once finished..
+//       var rawLog = reader.result.split(',')[1]; //extract only thee file data part
+//       var dataSend = { dataReq: { data: rawLog, name: file.name, type: file.type }, fname: "uploadFilesToGoogleDrive" }; //preapre info to send to API
+//       fetch('https://script.google.com/macros/s/AKfycbxu-1wyiW96S291l3VKk3rOpGhmNwST9VAzt3arrRyzUih56MiVVMJvm_QH426bdP9vrg/exec', //your AppsScript URL
+//         { method: "POST", body: JSON.stringify(dataSend) }) //send to Api
+//         .then(res => res.json()).then((a) => {
+//           console.log(a)
+//           setImgUrl(a) //See response
+//         }).catch(e => console.log(e)) // Or Error in console
+    
+//   }
+// }
+  // const submitHandler = (e) => {
+  //     e.preventDefault();
+  //     console.log({name,age,address,imgUrl,phoneNo,email});
+  //     axios.post("http://localhost:3000/data",{name,age,address,imgUrl,phoneNo,email,pdfUrl})
+  // }
   return (
     <div className="App">
-      <form onSubmit={(e) => submitHandler(e)}>
-      {/* <form> */}
+      <h1>Fill this form to join AZURE family</h1>
+      <form onSubmit={(e) => submitHandlerSheet(e)}>
         <input
           type="text"
           name="name"
           placeholder="enter your name"
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => changeHandler(e)}
         ></input>
         <input
           type="number"
           name="age"
           placeholder="enter your age"
-          onChange={(e) => setAge(e.target.value)}
+          onChange={(e) => changeHandler(e)}
         ></input>
         <input
           type="text"
           name="address"
           placeholder="enter your address"
-          onChange={(e) => setAddress(e.target.value)}
+          onChange={(e) => changeHandler(e)}
         ></input>
         <input
           type="text"
           name="phone"
           placeholder="enter your phone"
-          onChange={(e) => setPhoneNo(e.target.value)}
+          onChange={(e) => changeHandler(e)}
         ></input>
         <input
           type="text"
           name="email"
           placeholder="enter your email"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => changeHandler(e)}
         ></input>
         <input type={'file'} onChange={(e)=>ImageUpload(e.target.files[0])}></input>
-        <input type={'file'} onChange={(e)=>PdfUpload(e.target.files[0])}></input>
-        <input type={"submit"}></input>
+        <input type={'file'} onChange={(e)=>PdfUpload(e)}></input>
+        <input disabled={isPending?true:false} type={"submit"}></input>
       </form>
       {isPending&&<div>loading...</div>}
       {error&&<div>{errMsg}</div>}
--- src/CandidateList.js
+++ src/CandidateList.js
@@ -14,16 +14,20 @@ export default function CandidateList() {
                 <th>Address</th>
                 <th>Phone No</th>
                 <th>Email</th>
+                <th>Resume</th>
             </tr>
         </thead>
         <tbody>
         {data&&data.map((item)=>(
             <tr key={item.id}>
-                <td><img src={item.imgUrl} alt={item.name + "photo"} /></td>
+                <td className='img-container'><img src={item.imgUrl} alt={item.name + "photo"} /></td>
                 <td>{item.name}</td>
                 <td>{item.age}</td>
+                <td>{item.address}</td>
                 <td>{item.phoneNo}</td>
                 <td><a href={`mailto:${item.email}`}>{item.email}</a></td>
+                {/* <td><a target="_blank" href={item.pdfUrl} >resume</a></td> */}
+                <td><a href={item.pdfUrl.url} target='_blank'>click</a></td>
             </tr>
         ))}
         </tbody>
